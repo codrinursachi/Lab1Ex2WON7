28df17c61fbc11c5b17f3948cbadc041351e5843
codri <codri@VIRTUAL>
2024-02-15 22:42:23 +0200

    Add project files.

---
+++ Lab1Ex2WON7/Program.cs
@@ -0,0 +1,5 @@
+﻿/*Ex2
+Scrieti un program care va calcula media aritmetica a trei numere citite de la 
+tastatura*/
+const uint length=3;
+ShowAverage(ComputeAverage(ReadNumbers(length)));
---
+++ Lab1Ex2WON7/ProgramHelpers.cs
@@ -0,0 +1,34 @@
+﻿partial class Program
+{
+    static int[] ReadNumbers(uint length)
+    {
+        var numere= new int[length];
+        Console.WriteLine($"Introduceti {length} numere separate de un spatiu");
+        var numereRaw = Console.ReadLine()?.Split(" ");
+        if (numereRaw?.Length == length)
+            for (int i = 0; i < length; i++)
+            {
+                if (!int.TryParse(numereRaw[i], out numere[i]))
+                {
+                    Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+                    return ReadNumbers(length);
+                }
+            }
+        else
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            return ReadNumbers(length);
+        }
+        return numere;
+    }
+    static decimal ComputeAverage(int[] numbers)
+    {
+        decimal average;
+        average=(decimal)numbers.Sum()/numbers.Length;
+        return average;
+    }
+    static void ShowAverage(decimal average)
+    {
+        Console.WriteLine(average);
+    }
+}
