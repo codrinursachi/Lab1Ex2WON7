4e54bc92f5fa4cfdbed8fce3be3039da27449bcb
codrinursachi <codrinursachi@gmail.com>
2024-02-17 09:29:27 +0200

    Formatting

--- Lab1Ex2WON7/Program.cs
+++ Lab1Ex2WON7/Program.cs
@@ -3,5 +3,5 @@
 Scrieti un program care va calcula media aritmetica a trei numere citite de la 
 
 tastatura*/
-const uint LENGTH=3;
-ShowAverage(ComputeAverage(ReadNumbers(LENGTH)));
+const uint NUMAR_ELEMENTE=3;
+ShowAverage(ComputeAverage(ReadNumbers(NUMAR_ELEMENTE)));
